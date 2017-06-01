7e0047594883446c924fc79fc18a11d6de6bd53c
subhachandrachandra <schandra.tech@gmail.com>
2017-05-31 23:33:22 -0700

    NestTemp.py now prints all the sensor values in a CSV format.

--- NestTemp.py
+++ NestTemp.py
@@ -6,21 +6,17 @@ access_token_cache_file = 'nest.json'
 napi = nest.Nest(client_id=clientkeys.client_id, client_secret=clientkeys.client_secret, access_token_cache_file=access_token_cache_file)
 
 for structure in napi.structures:
-    print ('Structure %s' % structure.name)
-    print ('    Away                           : %s' % structure.away)
-    print ('    Postal Code                    : %s' % structure.postal_code)
-    print ('    Country                        : %s' % structure.country_code)
-    print ('    num_thermostats                : %s' % structure.num_thermostats)
-    print ('    Devices:')
-
     for device in structure.thermostats:
-        print ('        Device: %s' % device.name)
-        print ('        Where : %s' % device.where)
-        print ('            Temp       : %0.1f' % device.temperature)
-        print ('            Mode       : %s' % device.mode)
-        print ('            Fan        : %s' % device.fan)
-        print ('            Humidity   : %0.1f%%' % device.humidity)
-        print ('            Target     : %0.1fC' % device.target)
-        print ('            HVAC_STATE : %s' % device.hvac_state )
-        print ('            Mode       : %s' % device.mode )
-
+        print "%s,%s,%s,%s,%s,%0.1f,%0.1f%%,%0.1f,%s,%s,%s,%s" % ( 
+            structure.name,
+            structure.country_code,
+            structure.postal_code,
+            device.name,
+            device.where,
+            device.temperature,
+            device.humidity,
+            device.target,
+            device.mode,
+            device.fan,
+            device.hvac_state,
+            structure.away )
