9f194759be191ad4be6cbe50c28c00b750835ebe
subhachandrachandra <schandra.tech@gmail.com>
2017-06-03 00:18:07 -0700

    Fixes to get the environment setup correctly inside the container. Add a timestamp to sensor readings.

--- NestTemp.py
+++ NestTemp.py
@@ -1,22 +1,26 @@
-#! /usr/bin/env python
+#!/usr/local/bin/python3
 import nest
-import clientkeys
+import nest.clientkeys
+import time
 
-access_token_cache_file = 'nest.json'
-napi = nest.Nest(client_id=clientkeys.client_id, client_secret=clientkeys.client_secret, access_token_cache_file=access_token_cache_file)
+access_token_cache_file = '/root/nest.json'
+napi = nest.Nest(client_id=nest.clientkeys.client_id, 
+                 client_secret=nest.clientkeys.client_secret, 
+                 access_token_cache_file=access_token_cache_file)
 
 for structure in napi.structures:
     for device in structure.thermostats:
-        print "%s,%s,%s,%s,%s,%0.1f,%0.1f%%,%0.1f,%s,%s,%s,%s" % ( 
-            structure.name,
-            structure.country_code,
-            structure.postal_code,
-            device.name,
-            device.where,
-            device.temperature,
-            device.humidity,
-            device.target,
-            device.mode,
-            device.fan,
-            device.hvac_state,
-            structure.away )
+        print ( "%d,%s,%s,%s,%s,%s,%0.1f,%0.1f%%,%0.1f,%s,%s,%s,%s" % ( 
+                int(time.time()),
+                structure.name,
+                structure.country_code,
+                structure.postal_code,
+                device.name,
+                device.where,
+                device.temperature,
+                device.humidity,
+                device.target,
+                device.mode,
+                device.fan,
+                device.hvac_state,
+                structure.away ) )
